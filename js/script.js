377fdc87989972e7914245e0e0316429a731ad4f
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-05-28 18:48:34 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: May 2024
 // This file contains the JS functions for index.html
 
-const 
+function myButtonClicked() {
+  // variables
+  const NUMERATOR = 4
+  let denominator = 1
+  let answer = NUMERATOR / denominator
+  let operator = 1
+  let output = " (4 / 1)"
+  document.getElementById("output").innerHTML = null
+  document.getElementById("answer").innerHTML = null
+
+  // input
+  const numberOfTimes = parseInt(document.getElementById("number-of-times").value)
+
+  // process
+  for (counter = 1; counter < numberOfTimes; counter++) {
+    operator *= -1
+    denominator += 2
+    answer += (operator * NUMERATOR) / denominator
+    if (operator < 0) {
+      output += " - " + "(" + NUMERATOR + " / " + denominator + ")"
+    } else {
+      output += " + " + "(" + NUMERATOR + " / " + denominator + ")"
+    }
+  }
+
+  // output
+  document.getElementById("output").innerHTML = output
+  document.getElementById("answer").innerHTML = " = " + answer
+}
